3d7920fadf8a45a6bd82734bbb83938329661b89
Geneieve MacKenzie <mackenzie.gen@gmail.com>
2019-08-25 21:29:55 -0600

    final edits

--- app/Models/List.js
+++ app/Models/List.js
@@ -1,4 +1,41 @@
+
+
+
+
 export default class List {
+    constructor(data) {
+        this.name = data.name
+        this.listItem = data.listItem || []
+    }
+
+    getTemplate(index) {
+        let template = `
+            <div class="col-4 border p-3 m-auto">
+                <button type='btn-sm' onclick='app.controllers.listController.deleteList(${index})'>X</button>
+                <H1>${this.name}</H1>
+                `
+        template += this.drawItems(index)
+        template += `
+                <form onsubmit = "app.controllers.listController.addItem(event, ${index})">
+                    <div class= "form-group">
+                        <input type="text" class="form-control" name="item" placeholder='Add Item' required>
+                    </div>
+                <button class="btn-sm btn-danger" type= "submit">add item</button>
+                </form>
+            </div >
+            `
+        return template
+    }
+
+    drawItems(listIndex) {
+        let itemTemplate = ''
+        this.listItem.forEach((item, itemIndex) => {
+            itemTemplate += `<div class="row"><dl>${item}<span onclick='app.controllers.listController.deleteItem(${listIndex}, ${itemIndex})'><i class="fas fa-times deletebutton"></i></span></div>`
+        });
+        return itemTemplate
+    }
+
+
     //TODO You will need to create a constructor 
     //and the methods needed to create the view template for this model
 }
--- app/Services/ListService.js
+++ app/Services/ListService.js
@@ -11,9 +11,41 @@ export default class ValuesService {
     //TODO  Here is where we handle all of our data manipulation, 
     //given the information you need in the controller, 
     //what methods will be required to support that functionality?
+    constructor() {
+        console.log("Hi from List Service")
+        this.getLists()
+        this.saveLists()
+    }
+    addList(newList) {
+        _state.lists.push(new List(newList))
+        this.saveLists()
+        
+    }
+
+    deleteList(index) {
+        this.saveLists()
+        if (window.confirm("Are you sure you want to delete list?")) { 
+            _state.lists.splice(index, 1)
+        }
+        this.saveLists()
+    }
+
+    addItem(listIndex, newItem) {
+        _state.lists[listIndex].listItem.push(newItem)
+        this.saveLists()
+    }
+
+    deleteItem(listIndex, itemIndex) {
+        if (window.confirm("Are your sure you want to delete item?")) { 
+            _state.lists[listIndex].listItem.splice(itemIndex, 1)
+        }
+        this.saveLists()
+    }
 
 
-
+    get List() {
+       return  _state.lists.map(li => new List(li))
+    }
 
     //NOTE You will need this code to persist your data into local storage, these methods should not require changing
 
